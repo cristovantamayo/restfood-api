e371bf0c814513b99920decc1a8e24cd3a014b14
Cristovan Tamayo <ctamayo@bionexo.com>
2020-08-29 17:36:17 -0300

    Adding Project

---
+++ src/main/java/com/cristovantamayo/restfoodapi/RestfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.cristovantamayo.restfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristovantamayo/restfoodapi/RestfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristovantamayo.restfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
