80e47d8cbbe8d3ddf98340fbc61ee773860836ca
Cristovan Tamayo <ctamayo@bionexo.com>
2020-09-21 19:58:08 -0300

    New constraints & Cascading validation

--- src/main/java/com/cristovantamayo/restfoodapi/domains/cozinha/model/Cozinha.java
+++ src/main/java/com/cristovantamayo/restfoodapi/domains/cozinha/model/Cozinha.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 
 import com.cristovantamayo.restfoodapi.domains.restaurante.model.Restaurante;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -25,6 +26,7 @@ import lombok.Setter;
 @Entity
 public class Cozinha {
 	
+	@NotNull
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
