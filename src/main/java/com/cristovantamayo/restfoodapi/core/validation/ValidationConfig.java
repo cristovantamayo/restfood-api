e06f8fe1fc91838c07ffa1892a9e4e7c4ccd1f51
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2020-09-23 21:16:37 -0300

    messages.propertives as ValidationMessages.propertives

---
+++ src/main/java/com/cristovantamayo/restfoodapi/core/validation/ValidationConfig.java
@@ -0,0 +1,18 @@
+package com.cristovantamayo.restfoodapi.core.validation;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidationConfig {
+	
+	@Bean
+	public LocalValidatorFactoryBean validador(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+
+}
